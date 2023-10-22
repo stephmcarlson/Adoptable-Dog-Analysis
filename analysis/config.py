5fb85640e7332089fe8719bf55b66eb2a8951135
Alyssa Cullinan <alyssa.a.cullinan@gmail.com>
2023-10-22 18:39:55 -0500

    nearly complete analysis file

---
+++ analysis/config.py
@@ -0,0 +1,19 @@
+client_secret = "qtwlg41A2KQ3ysFbyAZoVX1aeVmbiRmrXo4wX6vN"
+api_key = "EQXBFuERrZkqZHaQlBLLScadjykmHLwPf1BR8NxyhcpU0Ch3ZA"
+geo_api_key = "6bf0e658ce8d4a2f9b7370aabb40dcaf"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
